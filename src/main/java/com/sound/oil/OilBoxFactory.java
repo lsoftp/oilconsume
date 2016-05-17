77932605a76793d6d9d7ed135c35edac4b547599
Liupeng <aliupeng@yahoo.com>
2016-05-17 17:26:50 +0800

    oilbox 7

--- src/main/java/com/sound/oil/OilBoxFactory.java
+++ src/main/java/com/sound/oil/OilBoxFactory.java
@@ -28,9 +28,7 @@ public class OilBoxFactory {
     public static final int SC1022DB4N4=23;
     public static final int SC1026DAN4=24;
     public static final int SC1034DD44=25;
-
-	
-	
+    public static final int JAC_XI_CENG_WHLG=26; //武汉理工江淮吸尘车
 
 	/**
 	 * @param oilBoxType
@@ -51,17 +49,20 @@ public class OilBoxFactory {
 			ob = new IrregularOilBox(10);
 			break;
 		case DFA1080SJ11D3:
-			ob = new OilBox10(10,3,10,1);
+			ob = new OilBox10(10,3,10,1);//TEST
 			break;
 		case DFD1O32GJ:  
 		case DFL1100BX7: 
 		case DFL1120B3:  
 		case DFL1160BX1V:
+			ob = new OilBox10(10,3,10,1);//TEST
+			break;
 		case DFL1160BX4: 
 		case DFL1160BX5:
-			ob = new OilBox10(5.79,6.11,4.9,1.3);
+			ob =  new OilBox10(5.79,6.14,4.94,1.368814);
 			break;
 		case DFL1160BXB:
+			break;
 		case DFL1250A11:
 			ob = new OilBox10(11.55,6.07,5.86,1.225);
 			break;
@@ -73,13 +74,28 @@ public class OilBoxFactory {
 		case QL1060A1HAY:
 		case QL10703HARY:
 		case QL1070A1ARY:
+			ob = new OilBox10(9.58,3.395,3.295,0.4951);
+			break;
 		case QL11009KARY:
+			ob = new OilBox10(9.52,3.36,3.21,0.314536);
+			break;
 		case QL11009LARY:
 		case SC1022DB4N4:
 		case SC1026DAN4:
-		case SC1034DD44:
 			ob = new BasalAreaOilBox(25.84538662);
 			break;
+		case SC1034DD44:
+			ob = new OilBox10(6.295,2.536,2.636,0.3087);
+			break;
+		case JAC_XI_CENG_WHLG:
+			ob = new OilBox10(9.32,4.06,3.24,0.535876);
+			break;
+//		case LWLDAR9G1FL002495:
+//			ob = new OilBox10(9.52,3.36,3.21,0.314536);
+//			break;
+//		case DFTJ185:
+//			ob = new OilBox10(5.79,6.14,4.94,1.368814);
+//			break;
 		default:
 			throw new Exception("invalid Oil box type:" + oilBoxType );
 		}
